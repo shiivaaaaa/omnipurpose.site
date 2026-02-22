03e2d2bf81c8e9261a7c2771f88f546627b6ab91
shiivaaaaa <shivasinghoff@outlook.com>
2026-02-22 18:30:32 +0530

    Update parser.js

--- tools/calculator/core/parser.js
+++ tools/calculator/core/parser.js
@@ -1,1 +1,11 @@
 
+export function safeEval(expr){
+expr = expr
+.replace(/π/g, Math.PI)
+.replace(/e/g, Math.E)
+.replace(/\^/g, "**")
+.replace(/×/g, "*")
+.replace(/÷/g, "/");
+
+return Function('"use strict"; return (' + expr + ')')();
+}
