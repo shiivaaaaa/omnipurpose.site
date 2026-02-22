7b67ef344a7d5374e926ee4dfbb74ce65ffd6f62
shiivaaaaa <shivasinghoff@outlook.com>
2026-02-22 16:24:28 +0530

    Update auth.js

--- tools/auth.js
+++ tools/auth.js
@@ -1,14 +1,20 @@
 const supabase = window.supabase.createClient(
-  "https://bhbdkarfpmvdbtdbsvmv.supabase.co",
-  "sb_publishable_yhjFWE2LvzK3n4omhwzFuA_N2blhp38"
+"https://bhbdkarfpmvdbtdbsvmv.supabase.co",
+"sb_publishable_yhjFWE2LvzK3n4omhwzFuA_N2blhp38"
 );
 
 async function checkAuth() {
-  const { data, error } = await supabase.auth.getUser();
 
-  if (!data.user) {
-    window.location.href = "/login.html";
-  }
+const {
+data: { session }
+} = await supabase.auth.getSession();
+
+if (!session) {
+
+window.location.href = "/login.html";
+
+}
+
 }
 
 checkAuth();
